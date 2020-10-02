65f94b1b16a36e40596e16e7ad3764cfd534b40b
Damien Tailhades <hello@regulardesigner.com>
2020-10-02 23:45:20 +0200

    :rotating_light: linter

--- src/mixins/indexedDB.js
+++ src/mixins/indexedDB.js
@@ -3,56 +3,56 @@ export const indexedDB = {
   // https://www.raymondcamden.com/2019/10/16/using-indexeddb-with-vuejs
 
   data() {
-    return{
-      db_name: 'videojournal',
+    return {
+      db_name: "videojournal",
       db_version: 1
-    }
+    };
   },
 
   methods: {
     async getIndexedDB() {
       return new Promise((resolve, reject) => {
-    
-      let request = window.indexedDB.open(this.db_name, this.db_version);
-      
-      request.onerror = e => {
-        console.log('Error Opening the indexed Database', e);
-        reject('Error');
-      };
-    
-      request.onsuccess = e => {
-        resolve(e.target.result);
-      };
-      
-      request.onupgradeneeded = e => {
-        console.log('On Upgrade Needed');
-        let db = e.target.result;
-        let objectStore = db.createObjectStore("videos", { autoIncrement: true, keyPath:'id' });
-      };
+        let request = window.indexedDB.open(this.db_name, this.db_version);
+
+        request.onerror = e => {
+          console.log("Error Opening the indexed Database", e);
+          reject("Error");
+        };
+
+        request.onsuccess = e => {
+          resolve(e.target.result);
+        };
+
+        request.onupgradeneeded = e => {
+          console.log("On Upgrade Needed");
+          let db = e.target.result;
+          let objectStore = db.createObjectStore("videos", {
+            autoIncrement: true,
+            keyPath: "id"
+          });
+        };
       });
     },
 
     async getVideosFromDb() {
       return new Promise((resolve, reject) => {
-    
-        let trans = this.db.transaction(['videos'],'readonly');
-        
+        let trans = this.db.transaction(["videos"], "readonly");
+
         trans.oncomplete = e => {
           resolve(videos);
         };
-        
-        let store = trans.objectStore('videos');
+
+        let store = trans.objectStore("videos");
         let videos = [];
-        
+
         store.openCursor().onsuccess = e => {
           let cursor = e.target.result;
-          
+
           if (cursor) {
-            videos.push(cursor.value)
+            videos.push(cursor.value);
             cursor.continue();
           }
         };
-    
       });
     },
 
@@ -64,44 +64,44 @@ export const indexedDB = {
         blob: blobFile
       };
 
-      console.log('about to add '+JSON.stringify(video));
-      
+      console.log("about to add " + JSON.stringify(video));
+
       await this.addVideoToDb(video);
-      
+
       this.savedVideos = await this.getVideosFromDb();
-      this.addDisabled = false;      
+      this.addDisabled = false;
     },
-    
+
     async addVideoToDb(video) {
       return new Promise((resolve, reject) => {
-        let trans = this.db.transaction(['videos'],'readwrite');
-        
+        let trans = this.db.transaction(["videos"], "readwrite");
+
         trans.oncomplete = e => {
           resolve();
         };
-        
-        let store = trans.objectStore('videos');
-        
+
+        let store = trans.objectStore("videos");
+
         store.add(video);
       });
     },
 
     async deleteVideo(id) {
-      console.log('console')
+      console.log("console");
       await this.deleteVideoFromDb(id);
-      this.savedVideos = await this.getVideosFromDb();      
+      this.savedVideos = await this.getVideosFromDb();
     },
 
     async deleteVideoFromDb(id) {
       return new Promise((resolve, reject) => {
-      let trans = this.db.transaction(['videos'],'readwrite');
-      trans.oncomplete = e => {
-        resolve();
-      };
-    
-      let store = trans.objectStore('videos');
-      store.delete(id);
+        let trans = this.db.transaction(["videos"], "readwrite");
+        trans.oncomplete = e => {
+          resolve();
+        };
+
+        let store = trans.objectStore("videos");
+        store.delete(id);
       });
-    },
+    }
   }
-}
+};
