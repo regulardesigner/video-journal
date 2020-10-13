543cf8c1954e89aca61df4993acd502cbfecba11
Damien Tailhades <hello@regulardesigner.com>
2020-10-13 23:10:59 +0200

    add store

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store'
+import './registerServiceWorker'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
+  store,
   render: h => h(App)
-}).$mount("#app");
+})
