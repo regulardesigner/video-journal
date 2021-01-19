d7fb346a4df58b7ce37378e5ad6df54a3a316a2e
Damien Tailhades <hello@regulardesigner.com>
2021-01-19 10:47:09 +0100

    deploy

---
+++ precache-manifest.09035f197ac77f6bb44f56ea8361dc99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef473014ac8f5ab8233d",
+    "url": "/video-journal/css/app.76947e68.css"
+  },
+  {
+    "revision": "d2747e56cb9007f3f234749b64e6c97e",
+    "url": "/video-journal/index.html"
+  },
+  {
+    "revision": "ef473014ac8f5ab8233d",
+    "url": "/video-journal/js/app.944b0ff3.js"
+  },
+  {
+    "revision": "e2f49d34efbc13069533",
+    "url": "/video-journal/js/chunk-vendors.531f13c5.js"
+  },
+  {
+    "revision": "55ad96f02cec001f95f7cc846ca7f672",
+    "url": "/video-journal/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/video-journal/robots.txt"
+  }
+]);
