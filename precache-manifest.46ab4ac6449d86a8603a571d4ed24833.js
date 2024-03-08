db316608b56f0cff2433ad57412fddbdd416f8ba
damien_tailhades <damien.tailhades@nextory.com>
2024-03-08 14:55:12 +0100

    deploy

---
+++ precache-manifest.46ab4ac6449d86a8603a571d4ed24833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "905e8a1d10b7ce30c7d6",
+    "url": "/video-journal/css/app.048ddb56.css"
+  },
+  {
+    "revision": "022757d245bcea311b9562fb65fc86f5",
+    "url": "/video-journal/index.html"
+  },
+  {
+    "revision": "905e8a1d10b7ce30c7d6",
+    "url": "/video-journal/js/app.a615a267.js"
+  },
+  {
+    "revision": "42c460c49c5433f4ac6c",
+    "url": "/video-journal/js/chunk-vendors.4341e09d.js"
+  },
+  {
+    "revision": "55ad96f02cec001f95f7cc846ca7f672",
+    "url": "/video-journal/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/video-journal/robots.txt"
+  }
+]);
